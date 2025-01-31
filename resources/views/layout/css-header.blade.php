94336bd61ff12e691b051e61c45cb23559e2ae3c
Dyaka Faiz <dyakafaiz123@gmail.com>
2025-01-31 20:27:54 +0700

    update

--- resources/views/layout/css-header.blade.php
+++ resources/views/layout/css-header.blade.php
@@ -1,23 +1,23 @@
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600;700;800&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600;700;800&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="{{ url('') }}/assets/css/bootstrap.css">
+    <link rel="stylesheet" href="{{ url('') }}/public/assets/css/bootstrap.css">
     
 
-    <link rel="stylesheet" href="{{ url('') }}/assets/vendors/iconly/bold.css">
+    <link rel="stylesheet" href="{{ url('') }}/public/assets/vendors/iconly/bold.css">
 
-    <link rel="stylesheet" href="{{ url('') }}/assets/vendors/perfect-scrollbar/perfect-scrollbar.css">
-    <link rel="stylesheet" href="{{ url('') }}/assets/vendors/bootstrap-icons/bootstrap-icons.css">
-    <link rel="stylesheet" href="{{ url('') }}/assets/css/app.css">
+    <link rel="stylesheet" href="{{ url('') }}/public/assets/vendors/perfect-scrollbar/perfect-scrollbar.css">
+    <link rel="stylesheet" href="{{ url('') }}/public/assets/vendors/bootstrap-icons/bootstrap-icons.css">
+    <link rel="stylesheet" href="{{ url('') }}/public/assets/css/app.css">
 
     {{-- Tab Icon --}}
     <link rel="shortcut icon" href="{{ url('') }}" type="image/x-icon">
 
     {{-- Sweet Alert --}}
-    <link rel="stylesheet" href="{{ url('') }}/assets/vendors/sweetalert2/sweetalert2.min.css">
+    <link rel="stylesheet" href="{{ url('') }}/public/assets/vendors/sweetalert2/sweetalert2.min.css">
     
     {{-- Toast --}}
-    <link rel="stylesheet" href="{{ url('') }}/assets/vendors/toastify/toastify.css">
+    <link rel="stylesheet" href="{{ url('') }}/public/assets/vendors/toastify/toastify.css">
 
     {{-- Data Table --}}
-    <link rel="stylesheet" href="{{ url('') }}/assets/vendors/simple-datatables/style.css">
+    <link rel="stylesheet" href="{{ url('') }}/public/assets/vendors/simple-datatables/style.css">
