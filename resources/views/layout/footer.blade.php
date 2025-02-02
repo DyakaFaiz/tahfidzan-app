5640046e60c225ed050a58dfb0006269cd265e9c
Dyaka Faiz <dyakafaiz123@gmail.com>
2025-02-02 09:53:11 +0700

    Update footer.blade.php

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -4,7 +4,7 @@
             <p><script>document.write(new Date().getFullYear())</script> &copy; BINNUS</p>
         </div>
         <div>
-            <small>0.0.1</small>
+            <small>0.0.2</small>
         </div>
         <div>
             <p>Crafted with <span class="text-danger"><i class="bi bi-heart"></i></span> by <a
