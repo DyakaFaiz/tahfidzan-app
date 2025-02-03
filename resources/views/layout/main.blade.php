ff33939c726a3ae68365c0e8858187c9a737a6c5
Dyaka Faiz <dyakafaiz123@gmail.com>
2025-02-03 12:58:50 +0700

    update icon

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -4,7 +4,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" type="image/png" href="{{ asset("assets/images/logo/logo.png") }}">
     <title>{{ $title }} | YANBU'UL QUR'AN 1</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
     @include('layout.css-header')
